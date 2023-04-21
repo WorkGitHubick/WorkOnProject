bf7706901cc0993134807e47adae942d38cb1a0f
WorkGitHubick <129836861+WorkGitHubick@users.noreply.github.com>
2023-04-22 02:51:04 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int result;
+
+Console.WriteLine("1 - Умножение\n2 - Деление\n3 - Сложение\n4 - Вычитание");
+
+Console.Write("Выберите действие: ");
+int choise = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
