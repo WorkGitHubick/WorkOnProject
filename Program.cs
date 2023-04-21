ec32978a45fb27757c57ae1457bf2ac2e730881d
Roman <work.name72343623@gmail.com>
2023-04-22 03:27:29 +0700

    Commit first function

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
-﻿int result;
+﻿int result = 0;
 
 Console.WriteLine("1 - Умножение\n2 - Деление\n3 - Сложение\n4 - Вычитание");
 
 Console.Write("Выберите действие: ");
 int choise = int.Parse(Console.ReadLine());
 
-Console.WriteLine();
+switch (choise)
+{
+    case 1:
+        result = Multi();
+        break;
+}
+
+Console.WriteLine($"Ответ: {result}");
+
+
+int Multi()
+{
+    int res;
+    Console.Write("Введите a: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Введите b: ");
+    int b = int.Parse(Console.ReadLine());
+    res = a * b;
+    return res;
+}
